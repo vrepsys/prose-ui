2f0e818f4a6430dff3971a5122986a878c7b690f
Valdemaras Repšys <valdemaras@gmail.com>
2024-12-10 12:15:11 +0200

    adjust landing page content and footer

--- apps/website/src/components/navigation/footer.tsx
+++ apps/website/src/components/navigation/footer.tsx
@@ -3,11 +3,24 @@ import { Logo } from './logo'
 export const Footer = () => (
   <div className="border-color-base text-color-lower fon-medium border-t">
     <div className="mx-auto grid w-full max-w-[var(--site-width)] grid-cols-1 gap-4 px-[var(--site-padding-x)] py-16">
-      <div className="prose-ui flex items-center gap-1">
+      <div className="flex items-center gap-1">
         <Logo />{' '}
-        <p className="text-color-low !my-0">
-          Created by <a href="#">Valdemaras</a>. Source code available on{' '}
-          <a href="https://github.com/valdemaras/prose-ui">GitHub</a>.{' '}
+        <p className="text-color-lowest !my-0 text-sm font-medium">
+          Created by{' '}
+          <a
+            className="text-color-low underline decoration-[hsl(var(--p-color-text-lowest))] underline-offset-4"
+            href="#"
+          >
+            Valdemaras
+          </a>
+          . Source code available on{' '}
+          <a
+            className="text-color-low underline decoration-[hsl(var(--p-color-text-lowest))] underline-offset-4"
+            href="https://github.com/valdemaras/prose-ui"
+          >
+            GitHub
+          </a>
+          .{' '}
         </p>
       </div>
     </div>

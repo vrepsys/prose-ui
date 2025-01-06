8691231b45da7387f03361eaa448881fd27888da
Valdemaras Repšys <valdemaras@gmail.com>
2025-01-06 18:59:50 +0200

    add editor to website

---
+++ apps/website/src/app/editor/page.tsx
@@ -0,0 +1,36 @@
+import { Footer } from '@/components/navigation/footer'
+import dhubDemo from '../../../public/img/dhub-demo.png'
+import { Metadata } from 'next'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export const metadata: Metadata = {
+  title: 'Editor - Prose UI',
+}
+
+const Templates = () => {
+  return (
+    <div>
+      <div className="mx-auto w-full max-w-[var(--site-width)] flex-1 items-center justify-between px-[var(--site-padding-x)]">
+        <div className="prose-ui pb-8">
+          <h1 className="mb-[var(--p-content-gap-cluster)] mt-[var(--p-content-gap-heading)] text-[length:var(--p-h1-font-size)] font-[var(--p-h1-font-weight)] leading-[var(--p-h1-line-height)] tracking-[var(--p-h1-letter-spacing)] text-[color:var(--p-h1-color)]">
+            Editor
+          </h1>
+          <p className="text-color-low">
+            Enable inline editing and collaboration for your MDX content.
+          </p>
+          <p className="mb-8">
+            Prose UI MDX is fully supported by <Link href="https://dhub.dev">Dhub</Link> - a
+            collaborative WYSIWYG editor for MDX content with direct GitHub publishing.
+          </p>
+          <Image
+            src={dhubDemo}
+            alt="Screenshot of Dhub's WYSIWYG editor with MarkDown preview open on the right side"
+          />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+export default Templates
--- apps/website/src/app/templates/page.tsx
+++ apps/website/src/app/templates/page.tsx
@@ -10,27 +10,31 @@ export const metadata: Metadata = {
 
 const Templates = () => {
   return (
-    <div className="mx-auto w-full max-w-[var(--site-width)] flex-1 items-center justify-between px-[var(--site-padding-x)]">
-      <h1 className="mb-[var(--p-content-gap-cluster)] mt-[var(--p-content-gap-heading)] text-[length:var(--p-h1-font-size)] font-[var(--p-h1-font-weight)] leading-[var(--p-h1-line-height)] tracking-[var(--p-h1-letter-spacing)] text-[color:var(--p-h1-color)]">
-        Templates
-      </h1>
-      <p>Get started with Prose UI templates.</p>
+    <div>
+      <div className="mx-auto w-full max-w-[var(--site-width)] flex-1 items-center justify-between px-[var(--site-padding-x)]">
+        <div className="prose-ui">
+          <h1 className="mb-[var(--p-content-gap-cluster)] mt-[var(--p-content-gap-heading)] text-[length:var(--p-h1-font-size)] font-[var(--p-h1-font-weight)] leading-[var(--p-h1-line-height)] tracking-[var(--p-h1-letter-spacing)] text-[color:var(--p-h1-color)]">
+            Templates
+          </h1>
+          <p className="text-color-low">Get started with Prose UI templates.</p>
+        </div>
 
-      <div className="mb-32 mt-[var(--p-content-gap-heading)] grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <TemplateCard
-          image={basicTemplate}
-          title="Basic starter"
-          description="Barebones starter template with Next.js 15, next/mdx, and Prose UI to render MDX content."
-          previewUrl="https://prose-ui-basic-starter.vercel.app"
-          sourceUrl="https://github.com/vrepsys/prose-ui-basic-starter"
-        />
-        <TemplateCard
-          image={docsTemplate}
-          title="Docs starter"
-          description="Documenatation starter with Next.js 15, Tailwind CSS, Content collections, Dark theme, and Prose UI for content."
-          previewUrl="https://prose-ui-docs-starter.vercel.app"
-          sourceUrl="https://github.com/vrepsys/prose-ui-docs-starter"
-        />
+        <div className="mb-32 mt-[var(--p-content-gap-heading)] grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <TemplateCard
+            image={basicTemplate}
+            title="Basic starter"
+            description="Barebones starter template with Next.js 15, next/mdx, and Prose UI to render MDX content."
+            previewUrl="https://prose-ui-basic-starter.vercel.app"
+            sourceUrl="https://github.com/vrepsys/prose-ui-basic-starter"
+          />
+          <TemplateCard
+            image={docsTemplate}
+            title="Docs starter"
+            description="Documenatation starter with Next.js 15, Tailwind CSS, Content collections, Dark theme, and Prose UI for content."
+            previewUrl="https://prose-ui-docs-starter.vercel.app"
+            sourceUrl="https://github.com/vrepsys/prose-ui-docs-starter"
+          />
+        </div>
       </div>
       <Footer />
     </div>
